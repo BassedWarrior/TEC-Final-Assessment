1734a6403a21ed41a7c066060c1cb90d60ca7f67
BassedWarrior <116653989+BassedWarrior@users.noreply.github.com>
2026-06-08 18:59:28 -0600

    fix(frontend): use Register component, not Login
    In order to differentiate it from the Login component rendered by the
    `login.tsx` route, use the Register component name.

--- frontend/app/routes/register.tsx
+++ frontend/app/routes/register.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router"
 
-export default function Login() {
+export default function Register() {
   const navigate = useNavigate()
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
